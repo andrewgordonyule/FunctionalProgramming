7bddd911a6299581206228ce7a4dc80bdf247268
andrewgordonyule <andrewgordonyule@gmail.com>
2022-10-24 22:58:17 +0100

    finish exercise sheet 1

--- exercises/exercise_sheet_1.hs
+++ exercises/exercise_sheet_1.hs
@@ -24,16 +24,26 @@ g :: (Int -> Bool) ->(Bool -> String) -> Int -> String
 g g1 g2 num = g2 (g1 num)
 
 g1 x = if x >= 18 then True else False
-g2 y = if y == True then "you can buy beer" else "you can'y buy beer"
+g2 y = if y == True then "you can buy beer" else "you can't buy beer"
 
--- 5
+-- 5. Write a function which applies the input function two times to the input integer
+twice :: (Int -> Int) -> Int -> Int
+twice func n = func (func n)
+
+-- helper function
+double :: Int -> Int
+double x = x * 2
+
+-- 6. Write a function to calculate the force between two masses m1 and m2 at distance d
 force :: Float -> Float -> Float -> Float
 force m1 m2 d = (g * m1 * m2) / (d ^ 2)
-    where g = 6.67 * (10 ^ (-11))
+    where 
+        g = 6.67 * (10 ^^ (-11))
 
 -- List Comprehensions
 
 -- 1. Using a list comprehension, calculate the numbers between 1 and 30 which are divisible by 3 (mod:: Int -> Int -> Int)
+divby3 :: [Int]
 divby3 = [x | x <- [1..30], x `mod` 3 == 0]
 
 -- 2. Using a list comprehension, write a function which calculates the first n triangle numbers
@@ -80,6 +90,14 @@ main = do
     -- 4
     print("g g1 g2 18")
     print(g g1 g2 18)
+
+    -- 5
+    print("twice")
+    print(twice double 2)
+
+    -- 6
+    print("force")
+    print(force 1.2 1.2 5)
 
     -- Lists
 
